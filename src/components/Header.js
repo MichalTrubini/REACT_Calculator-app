2bb3a247e218a18357055ab322bc6cfd832860bc
Michal Trubini <mchl.trubini@gmail.com>
2022-07-14 17:48:28 +0200

    first commit

---
+++ src/components/Header.js
@@ -0,0 +1,20 @@
+import '../css/header.css'
+
+const Header = () => {
+    return (
+        <>
+            <header className="header">
+                <div className="header__left">
+                    <p className='header__title'>calc</p>
+                </div>
+                <div className="header__right">
+                    <p className='header__subtitle'>theme</p>
+
+                </div>
+
+            </header>
+        </>
+    )
+}
+
+export default Header;
